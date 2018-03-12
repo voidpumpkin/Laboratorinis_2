2cae87aa176cea4c849153d2acbade078cd931b3
julys321 <32368314+julys321@users.noreply.github.com>
2018-03-12 23:16:57 +0200

    Create Uzduotis_6.java

---
+++ Uzduotis_6.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Labarotinis_2 {//Labarotinis_2_3_1  final num is iteration
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        List<List<Integer>> list = new ArrayList<List<Integer>>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<Integer>(m));
+            for (int j = 0; j < m; j++) {
+                list.get(i).add(scanner.nextInt());
+            }
+        }
+
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                sum += list.get(i).get(j);
+            }
+        }
+
+        System.out.printf("%.2f", sum / (m * n));
+    }
+}
