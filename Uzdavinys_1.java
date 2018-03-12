df24b70c3e8f83a18a76353a1173a0513240a727
julys321 <32368314+julys321@users.noreply.github.com>
2018-03-12 20:22:21 +0200

    Create Uzdavinys_1.java

---
+++ Uzdavinys_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Labarotinis_2 {//Labarotinis_2_1_1  final num is iteration
+    public static void main(String[] args) {
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        int n= scanner.nextInt();
+        for (int i = 1; i <= n; i++) {
+            sum += scanner.nextInt();
+        }
+        System.out.print(sum);
+    }
+}
