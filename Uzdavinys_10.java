ba1ce2a43605044273c29420ee850daec7408f48
julys321 <32368314+julys321@users.noreply.github.com>
2018-03-13 01:43:16 +0200

    Create Uzdavinys_10.java

---
+++ Uzdavinys_10.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Labarotinis_2 {//Labarotinis_2_9_1  final num is iteration
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<List<Integer>> list = new ArrayList<List<Integer>>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(new ArrayList<Integer>(n));
+            for (int j = 0; j < n; j++) {
+                list.get(i).add(scanner.nextInt());
+            }
+        }
+        if (n == 1) {
+            System.out.println("No");
+        } else {
+            int sum = 0;
+            for (int j = 0, i = 0; i < n; i++, j++) {
+                for (int k = 0; k < j; k++) {
+                    sum += list.get(i).get(k);
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
