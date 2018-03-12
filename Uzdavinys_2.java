43f4e46b61d7b098efbb2f1c953e308e517dca4c
julys321 <32368314+julys321@users.noreply.github.com>
2018-03-12 21:30:01 +0200

    Update Uzdavinys_2.java

--- Uzdavinys_2.java
+++ Uzdavinys_2.java
@@ -6,23 +6,24 @@ public class Labarotinis_2 {//Labarotinis_2_2_1  final num is iteration
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        
-        if (n < 2) {
-            System.out.println("No");
-        } else {
-            List<Integer> list = new ArrayList<Integer>(n * m);
-            for (int i = 0; i < n * m; i++) {
-                list.add(scanner.nextInt());
+        int n=scanner.nextInt();
+        List<Integer> list=new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            list.add(scanner.nextInt());
+        }
+        double sum=0;
+        double count=0;
+        for(int i:list){
+            if(i<0){
+                sum+=i;
+                count++;
             }
-            int biggestInt = list.get(m);
-            for (int i = m + 1; i < m * 2; i++) {
-                if (list.get(i) > biggestInt) {
-                    biggestInt = list.get(i);
-                }
-            }
-            System.out.print(biggestInt);
+        }
+        if(count==0){
+            System.out.println("NO");
+        }else {
+            double rezult=sum/count;
+            System.out.printf("%.2f",rezult);
         }
     }
 }
