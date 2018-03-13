1e719d17ba23dc376b625ea6a1eae3e02822551b
julys321 <32368314+julys321@users.noreply.github.com>
2018-03-13 10:05:32 +0200

    Update and rename Uzdavinys_13.java to Uzdavinys_14.java

---
+++ Uzdavinys_14.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Labarotinis_2 {//Labarotinis_2_14_1  final num is iteration
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<Integer> list = new ArrayList<Integer>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(scanner.nextInt());
+        }
+        Collections.sort(list);
+        int biggestInteger=-1000001;
+        boolean bigestNotDuplicateExists=false;
+        for (int i :list) {
+            if(biggestInteger<i && list.indexOf(i) == list.lastIndexOf(i)) {
+            biggestInteger=i;
+            bigestNotDuplicateExists=true;
+            }
+        }
+        if(bigestNotDuplicateExists) {
+            System.out.print(biggestInteger);
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
