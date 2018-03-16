9596a970eaf5a0969fa2273725a4316622dab969
julys321 <32368314+julys321@users.noreply.github.com>
2018-03-17 00:57:25 +0200

    Create Uzdavinys_17.java

---
+++ Uzdavinys_17.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Labarotinis_2 {//Labarotinis_2_17_1  final num is iteration
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int number;
+        int evenCount = 0;
+        int unevenCount = 0;
+        for (int i = 0; i < n; i++) {
+            number = scanner.nextInt();
+            if (number % 2 == 0) {
+                evenCount++;
+            } else {
+                unevenCount++;
+            }
+            if (evenCount >= 2) {
+                System.out.println("NELYGINIS");
+                break;
+            }
+            if (unevenCount >= 2) {
+                System.out.println("LYGINIS");
+                break;
+            }
+        }
+
+    }
+}
