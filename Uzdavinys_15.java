b819d8fe7322617c418a937de75b2d532eecb833
julys321 <32368314+julys321@users.noreply.github.com>
2018-03-18 19:35:30 +0200

    Update Uzdavinys_15.java

--- Uzdavinys_15.java
+++ Uzdavinys_15.java
@@ -3,20 +3,20 @@ package ProcedurinisProgramavimas;
 
 import java.util.*;
 
-public class Labarotinis_2 {//Labarotinis_2_15_2  final num is iteration
+public class Labarotinis_2 {//Labarotinis_2_15_3  final num is iteration
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = scanner.nextInt();
-        int n = scanner.nextInt();
+        long n = scanner.nextLong();
         System.out.print(executeTask(number, n));
     }
 
-    public static int executeTask(int number, int n) {
+    public static int executeTask(int number, long n) {
         List<Integer> list;
         int min, max;
-
-        for (int a = 1; a < n; a++) {
+        int copyOfNumber=0;
+        for (long a = 1; a < n; a++) {
             list = new ArrayList<Integer>();
             list.add(number / 1000);
             number = number - list.get(0) * 1000;
@@ -26,12 +26,16 @@ public class Labarotinis_2 {//Labarotinis_2_15_2  final num is iteration
             number = number - list.get(2) * 10;
             list.add(number);
 
-
             Collections.sort(list);
             min = list.get(0) * 1000 + list.get(1) * 100 + list.get(2) * 10 + list.get(3);
             Collections.reverse(list);
             max = list.get(0) * 1000 + list.get(1) * 100 + list.get(2) * 10 + list.get(3);
 
+
+            if(copyOfNumber==max){
+                a=n;
+            }
+            copyOfNumber=max;
             number = max - min;
         }
         return number;
