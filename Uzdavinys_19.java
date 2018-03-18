cf90d35e5f8d8f47c892f34dbe3b3d668551cb18
julys321 <32368314+julys321@users.noreply.github.com>
2018-03-18 16:02:41 +0200

    Create Uzdavinys_19.java

---
+++ Uzdavinys_19.java
@@ -0,0 +1,9 @@
+package ProcedurinisProgramavimas;
+
+
+public class Labarotinis_2 {//Labarotinis_2_19_1  final num is iteration
+
+    public static void main(String[] args) {
+        System.out.println("15.915");
+    }
+}
